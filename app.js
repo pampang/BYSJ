6f005f64bfccd381ae8b3cd16c5e37452e15b230
PAMPANG <fspwz@foxmail.com>
2016-04-30 23:37:16 +0800

    修整内容

--- app.js
+++ app.js
@@ -25,31 +25,34 @@ app.set('view engine', 'ejs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-	secret: settings.cookieSecret,
-	key: settings.db, //cookie name
-	cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},  //cookie设为-1时，会导致无法获取session，因为已被删除
-	// 在cookie时间内，共用一个session。
-	store: new MongoStore({
-		db: settings.db,
-		host: settings.host,
-		port: settings.port
-	})
+  secret: settings.cookieSecret,
+  key: settings.db, //cookie name
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 30
+  }, //cookie设为-1时，会导致无法获取session，因为已被删除
+  // 在cookie时间内，共用一个session。
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port: settings.port
+  })
 }));
 app.use(flash());
 
 routes(app);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -57,25 +60,25 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
-app.listen(app.get('port'), function(){
-	console.log('Express server listening on port ' + app.get('port'));
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 })
